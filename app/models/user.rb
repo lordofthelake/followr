d216cbdb71fe760a436f544b28b13190dda97439
Kevin Chandler <im.kevin@me.com>
2015-11-05 21:44:52 -0800

    add back follow/hour rate limit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,11 @@ class User < ActiveRecord::Base
 
   has_many :twitter_follows
   has_many :followers
-
   has_one :credential, dependent: :destroy
   has_one :twitter_follow_preference, dependent: :destroy
 
   scope :wants_twitter_follow, -> { joins('INNER JOIN twitter_follow_preferences ON (users.id = user_id)').where('twitter_follow_preferences.mass_follow IS TRUE') }
   scope :wants_twitter_unfollow, -> { joins('INNER JOIN twitter_follow_preferences ON (users.id = user_id)').where('twitter_follow_preferences.mass_unfollow IS TRUE') }
-  # scope :reauthentication_notification
 
   after_create :init_follow_prefs
 
@@ -44,11 +42,8 @@ class User < ActiveRecord::Base
     return false unless self.credential.is_valid
     return false if twitter_follow_preference.rate_limit_until > DateTime.now
 
-
-    # Disable due to twitter continuously blocking the app regardless of this
-    # followed_in_last_hour = self.twitter_follows.where('followed_at > ?', 1.hour.ago)
-    # followed_in_last_day = self.twitter_follows.where('followed_at > ?', 24.hours.ago)
-    # return false if followed_in_last_hour.count >= 30 || followed_in_last_day.count >= 720
+    followed_in_last_hour = self.twitter_follows.where('followed_at > ?', 1.hour.ago)
+    return false if followed_in_last_hour.count >= 50
     true
   end
 
