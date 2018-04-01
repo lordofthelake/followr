69a4d8d45cfb2f8ec688e5145e8ab6ad3bd52629
Michele Piccirillo <michele.piccirillo@gmail.com>
2018-04-01 23:54:22 +0200

    Show more relevant columns in admin page

--- app/admin/twitter_follow.rb
+++ app/admin/twitter_follow.rb
@@ -1,3 +1,24 @@
 ActiveAdmin.register TwitterFollow do
   belongs_to :user
+
+  index do
+    selectable_column
+    id_column
+    column :username do |tf|
+      link_to "@#{tf.username}", "https://twitter.com/#{tf.username}"
+    end
+    column :followed_at
+    column :unfollowed_at
+    column :following_count
+    column :followers_count
+    column :favourites_count
+    column :hashtag
+    column :was_following
+    column :lang
+    column :description
+    column :source_tweet_text
+    column :source_tweet_uri
+
+    actions
+  end
 end
